598571b931fecc19e8968c620ae23fbbba853092
Jan Kovar <honzik76kovar@gmail.com>
2025-06-05 15:23:35 +0200

    FE basic layout

---
+++ frontend/src/pages/index.tsx
@@ -0,0 +1,14 @@
+
+import React from 'react';
+import Layout from '../componenets/Layout';
+import PlayerSignUpForm from '../componenets/PlayerSignUpForm';
+
+const Home = () => {
+  return (
+      <Layout>
+        <PlayerSignUpForm/>
+      </Layout>
+  );
+};
+
+export default Home;
